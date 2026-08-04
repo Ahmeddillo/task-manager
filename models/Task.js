06dc8bf0abb06336672fd084bcb8e1a5876a8741
Ahmed Dillo <142218167+Ahmeddillo@users.noreply.github.com>
2026-08-04 13:39:00 +0300

    Create Task model with fields and default values

---
+++ models/Task.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/db');
+
+const Task = sequelize.define('Task', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  status: {
+    type: DataTypes.ENUM('yapılacak', 'yapılıyor', 'bitti'),
+    defaultValue: 'yapılacak',
+  },
+});
+
+module.exports = Task;
